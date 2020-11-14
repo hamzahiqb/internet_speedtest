37e2d271dec4591c4d0414ed2515c0776a9b7494
Hamzah Iqbal <hamzaiqb@amazon.com>
2020-11-14 18:49:25 +0000

    updates

--- runner.sh
+++ runner.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
-
+export PYENV_ROOT="$HOME/.pyenv"
+export PYENV_VIRTUALENV_DISABLE_PROMPT=1
+export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
+export PATH="$HOME/.poetry/bin:$HOME/Library/Caches/pypoetry/virtualenvs/internet-speedtest-dDhD3n-b-py3.8/bin:$PATH"
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-poetry run python "$DIR/speedtest_logger.py"
+
+poetry run python speedtest_logger.py -d $DIR
--- speedtest_logger.py
+++ speedtest_logger.py
@@ -1,9 +1,12 @@
+import argparse
 import csv
+import os
 import speedtest
 
 def write_dict(filename, dict_data):
 
-    with open(filename, "a+") as f:
+    mode = "a+" if os.path.exists(filename) else "w+"
+    with open(filename, mode) as f:
         w = csv.DictWriter(f, fieldnames=dict_data.keys())
 
         if f.tell() == 0:
@@ -11,21 +14,28 @@ def write_dict(filename, dict_data):
 
         w.writerow(dict_data)
 
+# Get path where files are saved
+parser = argparse.ArgumentParser()
+parser.add_argument("-d", nargs="?", default="~")
+args = parser.parse_args()
 
+# Run speed test
 st = speedtest.Speedtest()
 server = st.get_best_server()
 st.download()
 st.upload()
 
-
+# Parse and separate data
 results = st.results.dict()
 del results["client"]
 del results["server"]
 
+# Add timestamp to all as unique identifier
 client = dict(st.results.client)
 client["timestamp"] = results["timestamp"]
 server["timestamp"] = results["timestamp"]
 
-write_dict("server.csv", server)
-write_dict("client.csv", client)
-write_dict("results.csv", results)
+# Save speed test
+write_dict(f"{args.d}/server.csv", server)
+write_dict(f"{args.d}/client.csv", client)
+write_dict(f"{args.d}/results.csv", results)
